ad49f6a712bd171c403a1fefbe0d8beeb923bdbd
Matheqs <matheqs@outlook.com>
2024-06-11 01:20:27 -0300

    Buttons and sound interactions

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,11 +1,27 @@
 var buttonCollor = ["red", "blue", "green", "yellow"];
 var randomChosenCollor = buttonCollor[nextSequence()];
-var gamePattern = []
+var gamePattern = [];
+var gameOn = false;
+var userChosenCollor
+var userClicekdPattern = []
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
     return randomNumber
 };
 
-gamePattern.push(randomChosenCollor);
-console.log(randomChosenCollor);
+$("body").keypress(function(){
+    if (!gameOn){
+        gameOn = true;
+        gamePattern.push(randomChosenCollor);
+        var audio = $("#" + randomChosenCollor + "_audio")[0];
+        audio.play();
+        $("#" + randomChosenCollor).fadeOut(200).fadeIn(200);
+    }
+})
+
+$('div[type="button"]').click(function(){
+    userChosenCollor = $(this).attr('id');
+    userClicekdPattern.push(userChosenCollor)
+    console.log(userClicekdPattern)
+})
