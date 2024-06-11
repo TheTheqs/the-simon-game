2d07e0c00c1c2de43598d6d341b0a36637ce3e38
Matheqs <matheqs@outlook.com>
2024-06-11 00:31:36 -0300

    Adding random color selection system

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,3 +1,11 @@
-function nexSequence(){
-    
-}
+var buttonCollor = ["red", "blue", "green", "yellow"];
+var randomChosenCollor = buttonCollor[nextSequence()];
+var gamePattern = []
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber
+};
+
+gamePattern.push(randomChosenCollor);
+console.log(randomChosenCollor);
