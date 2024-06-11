9558391fd8b72279484d8d566277e03efcb432b5
Matheqs <matheqs@outlook.com>
2024-06-11 01:28:34 -0300

    Add sounds to button clicks

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,26 +2,32 @@ var buttonCollor = ["red", "blue", "green", "yellow"];
 var randomChosenCollor = buttonCollor[nextSequence()];
 var gamePattern = [];
 var gameOn = false;
-var userChosenCollor
-var userClicekdPattern = []
+var userChosenCollor;
+var userClicekdPattern = [];
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
-    return randomNumber
+    return randomNumber;
 };
+
+function playSound(name){
+    var audio = $("#" + name + "_audio")[0];
+    audio.play();
+}
 
 $("body").keypress(function(){
     if (!gameOn){
         gameOn = true;
         gamePattern.push(randomChosenCollor);
-        var audio = $("#" + randomChosenCollor + "_audio")[0];
-        audio.play();
+        playSound(randomChosenCollor);
         $("#" + randomChosenCollor).fadeOut(200).fadeIn(200);
     }
 })
 
 $('div[type="button"]').click(function(){
     userChosenCollor = $(this).attr('id');
-    userClicekdPattern.push(userChosenCollor)
-    console.log(userClicekdPattern)
+    userClicekdPattern.push(userChosenCollor);
+    playSound(userChosenCollor);
+    console.log(userClicekdPattern);
 })
+
